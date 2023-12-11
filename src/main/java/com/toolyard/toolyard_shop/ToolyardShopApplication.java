0eac06317eaa8f7ed1684860d5d29cb0880f32d1
Begli95 <saparov-begli@mail.ru>
2023-12-11 14:34:07 +0100

    first commit

---
+++ src/main/java/com/toolyard/toolyard_shop/ToolyardShopApplication.java
@@ -0,0 +1,13 @@
+package com.toolyard.toolyard_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolyardShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolyardShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toolyard/toolyard_shop/ToolyardShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolyard.toolyard_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolyardShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
